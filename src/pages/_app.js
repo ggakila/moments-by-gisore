f41d73400356ba2f46973262a1f0f6f13cf4e7fa
ggakila <gisorehaggai@gmail.com>
2023-09-20 23:24:11 +0300

    auth check

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 import "@/styles/globals.css";
+import { basePath } from "../../next.config";
 
 export default function App({ Component, pageProps: {session, ...pageProps} }) {
 	return (
